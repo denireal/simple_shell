e1b3ad0beab4bebe619bedf2e6785fd45785ab4f
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-11-13 03:40:10 -0800

    third tasks update

--- execute.c
+++ execute.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 /**
-* execute - Runs a command with its full path variables.
-* @data: A pointer to the program's data.
-* Return: Returns zero on success; otherwise, returns -1.
+* execute - This executes a command and its full path.
+* @nfo: A pointer to the program's info.
+* Return: Returns 0 on success; else returns -1.
 */
 int execute(progr_info *info)
 {
-int result = 0, status;
+int result = 0, mode;
 pid_t pidd;
 
 result =  search_exec_builtins(info);
@@ -34,11 +34,11 @@ perror(info->the_cmd), exit(EXIT_FAILURE);
 }
 else
 {
-wait(&status);
-if (WIFEXITED(status))
-errno = WEXITSTATUS(status);
-else if (WIFSIGNALED(status))
-errno = 128 + WTERMSIG(status);
+wait(&mode);
+if (WIFEXITED(mode))
+errno = WEXITSTATUS(mode);
+else if (WIFSIGNALED(mode))
+errno = 128 + WTERMSIG(mode);
 }
 }
 return (0);
