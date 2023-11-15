e29cb222923e36cf978dde460335956410c1b93e
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2023-11-14 23:56:34 -0800

    update for tx_getline.c

--- tx_getline.c
+++ tx_getline.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+#define MAX_INPUT_SIZE 1024
+
 /**
 * tx_getline - Function reads a line from the command prompt.
 * @progr_info: struct containing program information
@@ -8,14 +10,15 @@
 */
 int tx_getline(progr_info *info)
 {
-char buff[BUFFER_SIZE] = {'\0'};
+static char buffer[MAX_INPUT_SIZE] = {'\0'};
+static size_t buffer_position = 0;
 static char *cmd_input[10] = {NULL};
 static char cmd_oper[10] = {'\0'};
-ssize_t read_bytes, cmd_index = 0;
+ssize_t read_bytes;
+size_t cmd_index = 0;
 
-/* Check if there are no more commands in the array or if the previous
-* command was successful (based on logical operators)
-*/
+/* Check if there are no more commands in the array or if the previous */
+/* command was successful (based on logical operators) */
 if (!cmd_input[0] || (cmd_oper[0] == '&' && errno != 0) ||
 (cmd_oper[0] == '|' && errno == 0))
 {
@@ -26,15 +29,23 @@ free(cmd_input[cmd_index]);
 cmd_input[cmd_index] = NULL;
 }
 
-/* Read from the file descriptor fd into buff */
-read_bytes = read(info->fd, &buff, BUFFER_SIZE - 1);
-if (read_bytes == 0)
-return (-1);
+/* Read from the file descriptor fd into the buffer */
+if (buffer[buffer_position] == '\0')
+{
+read_bytes = read(info->fd, buffer, MAX_INPUT_SIZE - 1);
+if (read_bytes <= 0)
+{
+return (-1); /* Error or end of file */
+}
+buffer[read_bytes] = '\0';
+buffer_position = 0;
+}
 
 /* Split lines based on '\n' or ';' */
 for (cmd_index = 0; cmd_input[cmd_index]; cmd_index++)
 {
-cmd_input[cmd_index] = str_dup(_strtok(cmd_index ? NULL : buff, "\n;"));
+cmd_input[cmd_index] = str_dup(_strtok(cmd_index ? NULL : buffer +
+buffer_position, "\n;"));
 /* Check and split for '&&' and '||' operators */
 cmd_index = check_split_ops(cmd_input, cmd_index, cmd_oper);
 }
@@ -48,49 +59,8 @@ cmd_input[cmd_index] = cmd_input[cmd_index + 1];
 cmd_oper[cmd_index] = cmd_oper[cmd_index + 1];
 }
 
+/* Update buffer position */
+buffer_position += str_length(info->input_line);
+
 return (str_length(info->input_line));
-}
-
-/**
-* check_split_ops - Function checks and splits for the operators '&&' and '||'
-* @cmd_input: array of input commands
-* @cmd_index: index in cmd_input to be checked
-* @cmd_oper: array of logical operators for each previous command
-*
-* Return: index of the last command in cmd_input.
-*/
-int check_split_ops(char *cmd_input[], int cmd_index, char cmd_oper[])
-{
-char *temp = NULL;
-int char_index;
-
-/* This section check for the '&' character in the command line */
-for (char_index = 0; cmd_input[cmd_index] != NULL && cmd_input[cmd_index][char_index]; char_index++)
-{
-if (cmd_input[cmd_index][char_index] == '&' && cmd_input[cmd_index][char_index + 1] == '&')
-{
-/* This section split the line when '&&' is found */
-temp = cmd_input[cmd_index];
-cmd_input[cmd_index][char_index] = '\0';
-cmd_input[cmd_index] = str_dup(cmd_input[cmd_index]);
-cmd_input[cmd_index + 1] = str_dup(temp + char_index + 2);
-cmd_index++;
-cmd_oper[cmd_index] = '&';
-free(temp);
-char_index = 0;
-}
-if (cmd_input[cmd_index][char_index] == '|' && cmd_input[cmd_index][char_index + 1] == '|')
-{
-/* This section plit the line when '||' is found */
-temp = cmd_input[cmd_index];
-cmd_input[cmd_index][char_index] = '\0';
-cmd_input[cmd_index] = str_dup(cmd_input[cmd_index]);
-cmd_input[cmd_index + 1] = str_dup(temp + char_index + 2);
-cmd_index++;
-cmd_oper[cmd_index] = '|';
-free(temp);
-char_index = 0;
-}
-}
-return (cmd_index);
 }
